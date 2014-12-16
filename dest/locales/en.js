7d51718f7e9f5eac6a38a2490a890951f48fa29b
Ourai Lin <ourairyu@hotmail.com>
2014-12-16 16:36:37 +0800

    添加英文支持

---
+++ dest/locales/en.js
@@ -0,0 +1,12 @@
+Tatami.i18n({
+  _SystemDialog: {
+    title: "System",
+    button: {
+      ok: "OK",
+      cancel: "Cancel",
+      yes: "Yes",
+      no: "No",
+      close: "Close"
+    }
+  }
+});
---
+++ src/locales/en.coffee
@@ -0,0 +1,9 @@
+Tatami.i18n
+  _SystemDialog:
+    title: "System"
+    button:
+      ok: "OK"
+      cancel: "Cancel"
+      yes: "Yes"
+      no: "No"
+      close: "Close"
